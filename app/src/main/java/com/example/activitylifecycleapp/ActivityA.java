a8709ad7dec955e6157391a72d05e973780992b3
Varun Alla <varun.alla@sjsu.edu>
2022-02-18 17:54:09 -0800

    Open Activity B from Activity A.

--- app/src/main/java/com/example/activitylifecycleapp/ActivityA.java
+++ app/src/main/java/com/example/activitylifecycleapp/ActivityA.java
@@ -2,7 +2,9 @@ package com.example.activitylifecycleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityA extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ActivityA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+    }
+    public void btn_openActivityB(View view){
+        Intent activityB=new Intent(this,ActivityB.class);
+        startActivity(activityB);
     }
 }
