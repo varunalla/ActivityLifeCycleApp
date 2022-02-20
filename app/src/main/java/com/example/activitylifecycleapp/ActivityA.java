b567321a39570eaee46ac49fca39b9fbb232c13a
Varun Alla <varun.alla@sjsu.edu>
2022-02-19 18:13:16 -0800

    Added Activity C. Updated Lifecycle Events in Activity A, B

--- app/src/main/java/com/example/activitylifecycleapp/ActivityA.java
+++ app/src/main/java/com/example/activitylifecycleapp/ActivityA.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ActivityA extends AppCompatActivity {
@@ -12,9 +13,52 @@ public class ActivityA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        Log.e("onCreate ------ ", "ActivityA: onCreate()");
+
     }
     public void btn_openActivityB(View view){
         Intent activityB=new Intent(this,ActivityB.class);
         startActivity(activityB);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart ------ ","ActivityA: onStart()");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume ------ ","ActivityA: onResume()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("onPause ------ ","ActivityA: onPause()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("onStop ------ ","ActivityA: onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("onDestroy ------ ","ActivityA: onDestroy()");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("onRestart ------ ","ActivityA: onRestart()");
     }
 }
--- app/src/main/java/com/example/activitylifecycleapp/ActivityB.java
+++ app/src/main/java/com/example/activitylifecycleapp/ActivityB.java
@@ -3,6 +3,7 @@ package com.example.activitylifecycleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ActivityB extends AppCompatActivity {
 
@@ -10,5 +11,46 @@ public class ActivityB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        Log.e("onCreate ------ ", "ActivityB: onCreate()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart ------ ","ActivityB: onStart()");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume ------ ","ActivityB: onResume()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("onPause ------ ","ActivityB: onPause()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("onStop ------ ","ActivityB: onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("onDestroy ------ ","ActivityB: onDestroy()");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("onRestart ------ ","ActivityB: onRestart()");
     }
 }
